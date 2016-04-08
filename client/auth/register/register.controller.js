55a141e205a24b1d4456e61eaccc352eb245da34
Patrick Murphy <murphypatv@gmail.com>
2016-04-08 11:54:08 +0100

    display error message if all register form fields are not filled in

--- client/auth/register/register.controller.js
+++ client/auth/register/register.controller.js
@@ -20,6 +20,16 @@
 
 		vm.onSubmit = function () {
 			vm.formError = "";
+			if (!vm.credentials.name || !vm.credentials.email || !vm.credentials.password) {
+				vm.formError = "All fields required, please try again";
+				return false;
+			} else {
+				vm.doRegister();
+			}
+		};
+
+		vm.doRegister = function () {
+			vm.formError = "";
 			authentication
 				.register(vm.credentials)
 				.error(function(err) {
